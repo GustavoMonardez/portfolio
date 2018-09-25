c294cb540b7fd3a0036aa99c60fd13e9133e4c7c
Gustavo Monardez <gustavomonardez@hotmail.com>
2018-09-25 08:32:28 -0700

    removed contact form

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'GustavoMonardez@hotmail.com';
+$siteOwnersEmail = 'gustavomonardez@hotmail.com';
 
 
 if($_POST) {
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      	else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
 
